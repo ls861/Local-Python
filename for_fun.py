1e753f81fd09fa8b823a03d0fd3560b3fb2381b6
ls861 <53272723+ls861@users.noreply.github.com>
2020-01-27 18:00:34 +0000

    eod

---
+++ for_fun.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 10:41:48 2020
+
+@author: lester
+"""
+
+n=1000
+
+import numpy as np
+x = 3*np.random.random(n) # 100 random numbers
+q25, q50, q75 = np.percentile(x, [25, 50, 75])
+
+print(q25, q50, q75)
+
+print(sum(x[:250]/250))
+
+x = np.sort(x)
+
+print(x[499], x[500], (x[499]+x[500])/2)
+print(x[249], x[250], (x[249]+x[250])/2)
+
+
+y = sum(x)/n
+print(y)
