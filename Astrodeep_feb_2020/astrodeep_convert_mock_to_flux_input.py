34a7b69f0616ff90488ec60382d70c2d828fceef
ls861 <53272723+ls861@users.noreply.github.com>
2020-02-21 23:06:29 +0000

    usual

--- Astrodeep_feb_2020/astrodeep_convert_mock_to_flux_input.py
+++ Astrodeep_feb_2020/astrodeep_convert_mock_to_flux_input.py
@@ -7,58 +7,72 @@ Created on Tue Feb 11 12:56:05 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
 
-fileName = '/Users/lester/Documents/GitHub/Local-Python/Astrodeep_feb_2020/fit/mock_catalogue_005_009.fits'
+
+# =============================================================================
+# CALCULATE FLUXES FROM MAGNITUDE FITS FILE
+# =============================================================================
+
+
+filters = ['F090W_NRC_and_OTE_APP', 'F115W_NRC_and_OTE_APP', 'F150W_NRC_and_OTE_APP', 'F200W_NRC_and_OTE_APP', 'F277W_NRC_and_OTE_APP', 'F356W_NRC_and_OTE_APP', 'F444W_NRC_and_OTE_APP']
+
+filter_label = ['F090W', 'F115W', 'F150W', 'F200W', 'F277W', 'F356W', 'F444W']
+
+fileName = '/Users/lester/Documents/GitHub/Local-Python/Astrodeep_feb_2020/fit/mock_catalogue_005_010.fits'
 data_fits = fits.open(fileName)
-id_B = np.asarray(data_fits[1].data['ID'], dtype=int)
-z_B = data_fits[1].data['redshift_median']
-z_Berr = data_fits[1].data['redshift_68.00']
-#z_B = data_fits[1].data['redshift_mean']
+
+m = []
+for i in range(len(filters)):
+    m.append(np.array(data_fits['APPARENT MAGNITUDES'].data[filters[i]]))
+    
 data_fits.close()
 
 
-
-
-
-
-
-
-
-
-
-
-
+f = []
+for i in range(len(filters)):
+    f.append(10**( (23.9 - m[i]) / 2.5 ))
+    
+    
 
 # =============================================================================
-# filters = ['HST_ACS_WFC_F435W', 'HST_ACS_WFC_F606W', 'HST_ACS_WFC_F814W', 'HST_WFC3_IR_F105W', 'HST_WFC3_IR_F125W', 'HST_WFC3_IR_F140W', 'HST_WFC3_IR_F160W', 'Paranal_HAWKI_Ks', 'Spitzer_IRAC_I1', 'Spitzer_IRAC_I2']
+# FIND ERRORS
 # =============================================================================
+    
+sig_mag5 = np.array([29.4, 29.6, 29.7, 29.8, 29.4, 29.4, 29.1])
+    
+sig_flux5 = 10**( (23.9 - sig_mag5) / 2.5 )     # microJy
+    
+sig_flux = sig_flux5 / 5.
+    
+    
 
-header = ['b_B435', 'b_errB435', 'b_V606', 'b_errV606', 'b_I814', 'b_errI814', 'b_Y105', 'b_errY105', 'b_J125', 'b_errJ125', 'b_JH140', 'b_errJH140', 'b_H160', 'b_errH160', 'b_Ks', 'b_errKs', 'b_CH1', 'b_errCH1', 'b_CH2', 'b_errCH2', 'ZBEST', 'field', 'ID']
+# =============================================================================
+# Create ASCII file    
+# =============================================================================
+    
 
-header_string = '#ID b_B435 b_errB435 b_V606 b_errV606 b_I814 b_errI814 b_Y105 b_errY105 b_J125 b_errJ125 b_JH140 b_errJH140 b_H160 b_errH160 b_Ks b_errKs b_CH1 b_errCH1 b_CH2 b_errCH2 ZBEST field ID_original\n'
+header_string = '#ID F090W F115W F150W F200W F277W F356W F444W errF090W errF115W errF150W errF200W errF277W errF356W errF444W\n'
+
+file1 = open("JADES_ascii_001.txt","w+")
 
 
+file1.write(header_string)
 
-#f= open("astrodeep_ascii.txt","w+")  this was the original ~300, which doesn't include the ID to determine which galaxy
-#f= open("astrodeep_ascii2.txt","w+")   this was a subset of the above of just first 3 to fit locally
-
-f= open("astrodeep_ascii_002.txt","w+")  # this includes data to determine source galaxy, not used anywhere, can overwrite for now
-
-f.write(header_string)
-
-for i in range(len(D)):
+for i in range(len(m[0])):
     row = str(i+1)
     
-    for j in range(len(header)):
-        row = row + ' ' + str(D[header[j]][i])
-        
+    for j in range(len(filters)):
+        row = row + ' ' + str(f[j][i] + np.random.normal(loc=0.0, scale=sig_flux[j]))
+    
+    for j in range(len(sig_flux)):
+        row = row + ' ' + str(sig_flux[j])
+
     row = row + '\n'    
-    f.write(row)
+    file1.write(row)
 
-f.close()
-
-
-
+file1.close()
 
 
 
